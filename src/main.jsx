aaa6aa67df875e2cd89bd6ca41bdff26ccc8ffb5
simonquintana90 <83465824+simonquintana90@users.noreply.github.com>
2025-07-22 16:17:56 -0500

    Prueba Tailwind funcional

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- ASEGÚRATE DE QUE ESTA LÍNEA EXISTA
+import './index.css' // <-- Esta línea es esencial
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
