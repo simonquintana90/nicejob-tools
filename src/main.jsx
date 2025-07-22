343878f930dfd5f4daf7916be1d8189f9e0b03dd
simonquintana90 <83465824+simonquintana90@users.noreply.github.com>
2025-07-22 15:48:00 -0500

    Cambio en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- ESTA LÍNEA ES LA MÁS IMPORTANTE
+import './index.css' // <-- ASEGÚRATE DE QUE ESTA LÍNEA EXISTA
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
